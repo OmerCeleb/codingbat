5c18438a28d70fc0db7da405d97f0593a49ce78e
OmerCelebi123 <selambuben534@gmail.com>
2022-11-18 19:03:52 +0100

    coding-bat

---
+++ src/main/java/String1/nonStart.java
@@ -0,0 +1,24 @@
+package String1;
+
+public class nonStart {
+
+    /*
+Given 2 strings, return their concatenation, except omit the first char of each.
+The strings will be at least length 1.
+
+
+nonStart("Hello", "There") → "ellohere"
+nonStart("java", "code") → "avaode"
+nonStart("shotl", "java") → "hotlava"
+     */
+
+
+    public String nonStart(String a, String b) {
+
+
+        return a.substring(1, a.length()).concat(b.substring(1, b.length()));
+
+
+    }
+
+}
